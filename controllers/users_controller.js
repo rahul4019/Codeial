ad16a2d9dd4c097fe3353e144f9c64677b815d7e
Rahul <rahulkashyap4019@gmail.com>
2022-06-18 13:41:02 +0530

    completed signUp and signIn

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,24 @@
 const User = require("../models/user");
 
 module.exports.profile = function (req, res) {
-  return res.render("user_profile", {
-    title: "User Profile",
-  });
+  //   return res.render("user_profile", {
+  //     title: "User Profile",
+  //   });
+
+  if (req.cookies.user_id) {
+    User.findById(req.cookies.user_id, function (err, user) {
+      if (user) {
+        return res.render("user_profile", {
+          title: "User Profile",
+          user: user,
+        });
+      } else {
+        return res.redirect("/users/sign-in");
+      }
+    });
+  } else {
+    return res.redirect("/users/sign-in");
+  }
 };
 
 // render the sign up page
@@ -49,5 +64,27 @@ module.exports.create = function (req, res) {
 
 // sign in and create a session for the user
 module.exports.createSession = function (req, res) {
-  //todo later
+  // steps to authenticate
+
+  // find the user
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error in finding user in signing up");
+    }
+
+    // handle, if user is found
+    if (user) {
+      // handle password which doesn't match
+      if (user.password != req.body.password) {
+        return res.redirect("back");
+      }
+
+      // handle session creation
+      res.cookie("user_id", user.id);
+      return res.redirect("/users/profile");
+    } else {
+      // handle, if user not found
+      return res.redirect("back");
+    }
+  });
 };
