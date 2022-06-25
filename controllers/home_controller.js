a5d6d805ff331acc035fd7c4e700ca1388a0c22e
Rahul <rahulkashyap4019@gmail.com>
2022-06-25 09:26:48 +0530

    added comments on post functionality

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,27 +1,33 @@
-const Post = require('../models/post');
+const Post = require("../models/post");
 
-module.exports.home = function(req,res){
+module.exports.home = function (req, res) {
+  // console.log(req.cookies);
+  // res.cookie('user_id',25);
 
-    // console.log(req.cookies);
-    // res.cookie('user_id',25);
+  // Post.find({},function(err,posts){
 
-    // Post.find({},function(err,posts){
-        
-    //     return res.render('home',{
-    //         title:"Codial | Home",
-    //         posts: posts
-    //     });
-    // })
+  //     return res.render('home',{
+  //         title:"Codial | Home",
+  //         posts: posts
+  //     });
+  // })
 
-    // populate the user of each post
-    Post.find({}).populate('user').exec(function(err,posts){
-        return res.render('home',{
-            title:"Codial | Home",
-            posts: posts
-        });
+  // populate the user of each post
+  Post.find({})
+    .populate("user")
+    .populate({
+      path: "comments",
+      // nested pre populating
+      populate: {
+        path: "user",
+      },
     })
-
-
-}
+    .exec(function (err, posts) {
+      return res.render("home", {
+        title: "Codial | Home",
+        posts: posts,
+      });
+    });
+};
 
 // module.exports.actionName = function(res,req){}
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
@@ -8,6 +9,7 @@ console.log('router loaded');
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
+router.use('/comments',require('./comments'));
 
 // for any further routes, access from here
 // router.use('/routerName',requier('./routerFile)); 
