f5ecbcc3a59162fb853a65f0a9b25b651b6d6728
Rahul <rahulkashyap4019@gmail.com>
2022-06-27 08:13:46 +0530

    added post delete and post's comments delete funcnailty

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -4,19 +4,41 @@ const Post = require("../models/post");
 module.exports.create = function (req, res) {
   Post.findById(req.body.post, function (err, post) {
     if (post) {
-      Comment.create({
-        content: req.body.content,
-        post: req.body.post,
-        user: req.user._id
-      },function(err,comment){
-        // handle error
+      Comment.create(
+        {
+          content: req.body.content,
+          post: req.body.post,
+          user: req.user._id,
+        },
+        function (err, comment) {
+          // handle error
 
-        // push is given by mongodb
-        post.comments.push(comment);
-        post.save();
+          // push is given by mongodb
+          post.comments.push(comment);
+          post.save();
 
-        res.redirect('/');
-      });
+          res.redirect("/");
+        }
+      );
     }
   });
 };
+
+module.exports.destroy = function (req, res) {
+  Comment.findById(req.params.id, function (err, comment) {
+    if (comment.user == req.user.id) {
+      let postId = comment.post;
+
+      comment.remove();
+
+      // inbuilt feature
+      Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } },function(err,post){
+        return res.redirect('back');
+      })
+    }else{
+      return res.redirect('back');
+    }
+  });
+};
+
+
